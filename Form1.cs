7f100a8a8ff772e3b8e2dd2403cee50fd215842d
Maciej Kaczmarek <ul0253576@edu.uni.lodz.pl>
2022-04-22 23:42:48 +0200

    root commit

---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formsAPP
+{
+    public partial class Form1 : Form
+    {
+        Pracownik pracownik = new Pracownik();
+        //List<string> p = new List<string>();
+        PasswordValidation passwordValidation = new PasswordValidation();
+        int ilość_pracowników = 0;
+
+        public Form1()
+        {
+
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+
+
+        }
+
+        private void addButton_click(object sender, EventArgs e)
+        {
+
+
+            pracownik.p.Add(new Pracownik()
+            {
+                Id = (int)numericUpDown2.Value,
+                name = textBox1.Text,
+                surname = textBox2.Text,
+                age = (int)numericUpDown1.Value,
+                date = dateTimePicker1.Value,
+            });
+
+
+
+
+            pracownik.liczbaPracowników.Add(1);
+
+
+
+            dataGridView1.Rows.Add(textBox1.Text, textBox2.Text, numericUpDown1.Value.ToString());
+
+
+
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Visible = true;
+
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+
+
+
+
+
+            //textBox1.Text = dataGridView1.SelectedRows[0].Cells[0].ToString();
+
+
+
+        }
+
+        private void PasswordValidationButton_Click(object sender, EventArgs e)
+        {
+
+            passwordValidation.checkValidation(password: textBox3.Text);
+            label1.Visible = true;
+            label1.Text = "działa";
+
+  
+            if (passwordValidation.validationCorrect == false)
+            {
+                    label1.Visible = true;
+                    label1.Text = "nie działa";
+            }
+            passwordValidation.refreshCorrectPassword();
+
+            //passwordValidation.refreshCorrectPassword();
+
+        }
+
+
+
+        
+
+
+
+
+
+
+
+
+    }
+}
---
+++ PasswordValidation.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace formsAPP
+{
+    public class PasswordValidation
+    {
+        public string password { get; set; }
+
+        public bool correctPassword = false;
+
+        public bool validationCorrect = false;
+
+        public void checkValidation(string password)
+        {
+
+            if (passLenght(password) == true)
+            {
+
+                if (passUpperCase(password) == true)
+                {
+
+
+                    if (passLowerCase(password) == true)
+                    {
+                        if(passSpecialSign(password)== true)
+                        {
+                            if (passNumber(password) == true)
+                            {
+                                completePassword();
+
+                                // tu zwroci correctpassword na true
+
+
+                                validationCorrect = true;
+
+
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+
+        
+
+
+
+
+
+        public bool passLenght(string password)
+        {
+            if (password.Length >= 8 && password.Length <= 15)
+                return true;
+            else
+                
+                return false;
+        }
+        
+        public bool passUpperCase(string password)
+        {
+            if (password.Any(char.IsUpper))
+                return true;
+            else
+               
+            return false;
+
+        }
+
+        public bool passLowerCase(string password)
+        {
+            if (password.Any(char.IsLower))
+                return true;
+            else
+                
+            return false;
+
+        }
+
+        public bool passNumber(string password)
+        {
+            if (password.Any(char.IsNumber))
+                return true;
+            else
+                
+            return false;
+        }
+
+        public bool passSpecialSign(string password)
+        {
+            _ = Char.GetUnicodeCategory('!') == System.Globalization.UnicodeCategory.ModifierSymbol;
+
+            if (password.Any(char.IsSymbol))
+                return true;
+            else
+                
+            return false;
+
+        }
+
+    
+
+        public bool completePassword()
+        {
+            return correctPassword = true;
+        }
+
+        public bool refreshCorrectPassword()
+        {
+            return validationCorrect = false;
+        }
+    }
+}
